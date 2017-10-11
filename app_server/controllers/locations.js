f704f9b5aa9cd0210d9a3f32f2b5bc72be071a30
AlexandruSateanu <alex.sateanu@gmail.com>
2017-10-12 01:10:10 +0300

    Update controllers to post review using API. Validate form.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -84,7 +84,7 @@ module.exports.homelist = function (req, res) {
     }
   };
 
-  request(requestOptions, function(err, response, body) {
+  request(requestOptions, function (err, response, body) {
     var i, data;
     data = body;
 
@@ -98,7 +98,33 @@ module.exports.homelist = function (req, res) {
   });
 };
 
-var renderDetailPage = function(req, res, locDetail) {
+var getLocationInfo = function (req, res, callback) {
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+  
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {}
+  };
+
+  request(requestOptions, function (err, response, body) {
+    var data = body;
+
+    if (response.statusCode === 200) {
+      data.coords = {
+        lng: body.coords[0],
+        lat: body.coords[1]
+      };
+  
+      callback(req, res, data);
+    } else {
+      _showError(req, res, response.statusCode);
+    }
+  });
+};
+
+var renderDetailPage = function (req, res, locDetail) {
   res.render('location-info', {
     title: locDetail.name,
     pageHeader: {title: locDetail.name},
@@ -112,35 +138,55 @@ var renderDetailPage = function(req, res, locDetail) {
 
 /* GET 'Location Info' page. */
 module.exports.locationInfo = function (req, res) {
-  var requestOptions, path;
-  path = "/api/locations/" + req.params.locationid;
-  
-  requestOptions = {
-    url : apiOptions.server + path,
-    method : "GET",
-    json : {}
-  };
+  getLocationInfo(req, res, function (req, res, responseData) {
+    renderDetailPage(req, res, responseData);
+  });
+};
 
-  request(requestOptions, function(err, response, body) {
-    var data = body;
-
-    if (response.statusCode === 200) {
-      data.coords = {
-        lng: body.coords[0],
-        lat: body.coords[1]
-      };
-  
-      renderDetailPage(req, res, data);
-    } else {
-      _showError(req, res, response.statusCode);
-    }
+var renderReviewForm = function (req, res, locDetail) {
+  res.render('location-review-form', {
+    title: 'Review ' + locDetail.name + ' on Loc8r',
+    pageHeader: { title: 'Review ' + locDetail.name },
+    error: req.query.err
   });
 };
 
 /* GET 'Add Review' page. */
 module.exports.addReview = function (req, res) {
-  res.render('location-review-form', {
-    title: 'Review Starcups on Loc8r',
-    pageHeader: { title: 'Review Starcups' }
+  getLocationInfo(req, res, function (req, res, responseData) {
+    renderReviewForm(req, res, responseData);
   });
+};
+
+/* POST 'doAddReview' form. */
+module.exports.doAddReview = function (req, res) {
+  var requestOptions, path, locationid, postdata;
+  locationid = req.params.locationid;
+  path = "/api/locations/" + locationid + '/reviews';
+
+  postdata = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "POST",
+    json : postdata
+  };
+
+  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+    res.redirect('/location/' + locationid + '/review/new?err=val');
+  } else {
+    request(requestOptions, function (err, response, body) {
+      if (response.statusCode === 201) {
+        res.redirect('/location/' + locationid);
+      } else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
+        res.redirect('/location/' + locationid + '/review/new?err=val');
+      } else {
+        _showError(req, res, response.statusCode);
+      }
+    });
+  }
 };
