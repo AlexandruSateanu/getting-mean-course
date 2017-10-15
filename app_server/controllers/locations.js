f09f068ea7ad399d7d729ed408d2a67f91cb08cb
AlexandruSateanu <alex.sateanu@gmail.com>
2017-10-15 16:11:03 +0300

    Adding Angular to the locations page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -26,8 +26,8 @@ var _showError = function (req, res, status) {
   });
 };
 
-var renderHomepage = function(req, res, responseBody) {
-  var message;
+var renderHomepage = function(req, res) {
+  /*var message;
 
   if (!(responseBody instanceof Array)) {
     message = "API lookup error";
@@ -36,7 +36,7 @@ var renderHomepage = function(req, res, responseBody) {
     if (!responseBody.length) {
       message = "No places found nearby";
     }
-  }
+  }*/
 
   res.render('locations-list', { 
     title: 'Loc8r - find a place to work with wifi',
@@ -44,9 +44,7 @@ var renderHomepage = function(req, res, responseBody) {
       title: 'Loc8r',
       strapline: 'Find places to work with wifi near you!'
     },
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-    locations: responseBody,
-    message: message
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
   });
 };
 
@@ -70,7 +68,8 @@ var _formatDistance = function (distance) {
 
 /* GET 'home' page. */
 module.exports.homelist = function (req, res) {
-  var requestOptions, path;
+  /* Using Express*/
+  /*var requestOptions, path;
   path = '/api/locations';
 
   requestOptions = {
@@ -93,9 +92,10 @@ module.exports.homelist = function (req, res) {
         data[i].distance = _formatDistance(data[i].distance);
       }
     }
-
     renderHomepage(req, res, data);
-  });
+  });*/
+
+  renderHomepage(req, res);
 };
 
 var getLocationInfo = function (req, res, callback) {
@@ -147,7 +147,8 @@ var renderReviewForm = function (req, res, locDetail) {
   res.render('location-review-form', {
     title: 'Review ' + locDetail.name + ' on Loc8r',
     pageHeader: { title: 'Review ' + locDetail.name },
-    error: req.query.err
+    error: req.query.err,
+    url: req.originalUrl
   });
 };
 
